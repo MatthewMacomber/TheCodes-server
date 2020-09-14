284369f27979a109286a0f5a2fff6dd531b6e728
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-09-14 15:55:33 -0700

    demo account changes

--- seeds/seed.thecodes_tables.sql
+++ seeds/seed.thecodes_tables.sql
@@ -14,7 +14,7 @@ values
 
 insert into thecodes_users (user_name, full_name, nickname, password)
 values
-  ('matt', 'Matthew Macomber', 'Matt', '$2y$12$XN7iEugoKSJPFEoBQtikaOIfzZShAhWDDiGjBYAg0bgKsXuRz3zzG'), -- test password is: P4ssword!
+  ('demo', 'Matthew Macomber', 'Demo', '$2y$12$XN7iEugoKSJPFEoBQtikaOIfzZShAhWDDiGjBYAg0bgKsXuRz3zzG'), -- test password is: P4ssword!
   ('bob', 'Bob Builder', 'Bob', '$2y$12$XN7iEugoKSJPFEoBQtikaOIfzZShAhWDDiGjBYAg0bgKsXuRz3zzG'); -- test password is: P4ssword!
 
 insert into thecodes_codes (title, user_id, user_name, content, answer)
